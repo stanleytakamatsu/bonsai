6ccc807f28d3c4ec8025092c96a9446f369fcc81
Stanley Yoshinori Takamatsu <stanleytakamatsu@gmail.com>
2019-04-01 10:40:46 -0300

    Create find by email method

--- App/Account/Repository/IAccountRepository.ts
+++ App/Account/Repository/IAccountRepository.ts
@@ -1,7 +1,10 @@
+import { Account } from '../Entity/Account';
 import { ICreateAccountCommand } from '../Tyoe/Command/Repository/ICreateAccountCommand';
+import { IFindAccountByEmailQuery } from '../Tyoe/Query/Repository/IFindAccountByEmailQuery';
 
 interface IAccountRepository {
   create(command: ICreateAccountCommand): Promise<void>;
+  findByEmail(query: IFindAccountByEmailQuery): Promise<Account>;
 }
 
 const IAccountRepository = Symbol.for('IAccountRepository');
--- App/Account/Repository/Mongoose/AccountMongooseRepository.ts
+++ App/Account/Repository/Mongoose/AccountMongooseRepository.ts
@@ -1,9 +1,13 @@
 import { IMongooseConnection } from '../../../Core/Database/Driver/Mongoose/Connection/IMongooseConnection';
 import { MongooseRepository } from '../../../Core/Database/Driver/Mongoose/Repository/MongooseRepository';
 import { ConflictRecordError } from '../../../Core/Error/Repository/ConflictRecordError';
+import { RecordNotFoundError } from '../../../Core/Error/Repository/RecordNotFoundError';
 import { SaveRecordError } from '../../../Core/Error/Repository/SaveRecordError';
+import { Account } from '../../Entity/Account';
 import { ICreateAccountCommand } from '../../Tyoe/Command/Repository/ICreateAccountCommand';
+import { IFindAccountByEmailQuery } from '../../Tyoe/Query/Repository/IFindAccountByEmailQuery';
 import { IAccountRepository } from '../IAccountRepository';
+import { IAccountModel } from '../Model/IAccountModel';
 
 import { IAccountMongooseModel } from './Model/IAccountMongooseModel';
 import { AccountSchema } from './Schema/AccountSchema';
@@ -31,6 +35,24 @@ class AccountMongooseRepository extends MongooseRepository<IAccountMongooseModel
     } catch (error) {
       this.throwSpecificErrorBasedOn(error);
     }
+  }
+
+  public async findByEmail(query: IFindAccountByEmailQuery): Promise<Account> {
+    let record: IAccountModel;
+
+    try {
+      const filter: Partial<IAccountModel> = { email: query.Email };
+
+      record = await this.documentModel.findOne(filter).exec();
+    } catch (error) {
+      this.throwSpecificErrorBasedOn(error);
+    }
+
+    if (!record) {
+      throw new RecordNotFoundError('The record not found');
+    }
+
+    return Account.createFromRecord(record);
   }
 
   protected throwSpecificErrorBasedOn(error: any): void {
